2ae9a5ab703d6c06cf3f12f4990c93e7d01b254b
Rafael Pereira Laurindo <rplaurindo>
2019-12-07 02:42:45 -0300

    improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,49 +16,70 @@ import { NgPageBrowser } from '@rplaurindo/ng-page-browser';
 })
 export class AppComponent implements OnInit {
 
-    @ViewChild('pageBrowser', { static: false }) private pageBrowser: PageBrowserComponent;
+    @ViewChild('pageBrowser', { static: true }) private pageBrowser: PageBrowserComponent;
 
     enablePageNumberInputBox: boolean;
 
-    collection: object[];
+    collectionPromise: Promise<object[]>;
+
+    collectionPagination: NgPageBrowser.Pagination<object>;
 
     pageNumber: number;
 
     limit: number;
 
+    private page: object[];
+
+    private count: number;
+
     constructor() {
         this.enablePageNumberInputBox = true;
-        this.collection = [];
         this.pageNumber = 1;
         this.limit = 5;
-
-        const
-            interval = setInterval(
-                () => {
-                    const count = 10000;
-
-                    this.pageBrowser.totalPages = NgPageBrowser.Pagination.calculatesTotalPages(count, this.limit);
-
-                    for (let i = 1; i <= count; i++) {
-                        this.collection.push({
-                            property1: `property1 value ${i}`
-                            , property2: `property2 value ${i}`
-                        });
-                    }
-
-                    console.log('function called after an interval');
-                    clearInterval(interval);
-                }, 2000
-            );
-
     }
 
     ngOnInit() {
+        const collection: object[] = [];
+        this.count = 10000;
 
+        for (let i = 1; i <= this.count; i++) {
+            collection.push({
+                property1: `property1 value ${i}`
+                , property2: `property2 value ${i}`
+            });
+        }
+
+        this.collectionPagination = new NgPageBrowser.Pagination(collection, this.limit);
+
+        this.getPage();
     }
 
     onChangePage(pageNumber: number) {
         this.pageNumber = pageNumber;
+
+        this.getPage();
+    }
+
+    getPage() {
+        this.collectionPromise = new Promise(
+            (accomplish: (collection: object[]) => void) => {
+
+                const interval = setInterval(
+                    () => {
+                        this.page = this.collectionPagination.getPage(this.pageNumber);
+
+                        this.pageBrowser.totalPages = NgPageBrowser.Pagination
+                            .calculatesTotalPages(this.count, this.limit);
+
+                        accomplish(this.page);
+
+                        console.log('function called after an interval');
+                        clearInterval(interval);
+                    }, 1500
+                );
+
+            }
+        );
     }
 
 }
