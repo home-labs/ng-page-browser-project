b7ac4054c8121f8171cc2b3587fc17f465fbcb31
Rafael Pereira <rafaelplaurindo@gmail.com>
2021-03-02 13:52:22 -0300

    improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-// import { NgPageBrowserModule } from '@actjs.on/ng-page-browser';
-import { NgPageBrowserModule } from 'projects/ng-page-browser/public-api';
+import { NgPageBrowserModule } from '@actjs.on/ng-page-browser';
+// import { NgPageBrowserModule } from 'projects/ng-page-browser/public-api';
 
 
 @NgModule({
     imports: [
-        // BrowserModule,
-        // , NgPageBrowserModule
         NgPageBrowserModule
     ],
     declarations: [
